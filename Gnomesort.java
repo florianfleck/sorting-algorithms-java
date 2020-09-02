56863d19fa1b932eeaf67b65c0385075afeede02
Florian Fleck <70074489+florianfleck@users.noreply.github.com>
2020-09-02 16:13:09 +0200

    Add files via upload
    This program contains the Gnome Sort. The algorithmen sorts an one dimensional array, which consists of int numbers. It also prints the time to get the array sorted.

---
+++ Gnomesort.java
@@ -0,0 +1,59 @@
+
+public class Gnomesort
+{
+    
+    int Array[] = new int [100000];
+    
+    /**
+     * Konstruktor für Objekte der Klasse Gnomesort
+     */
+    public Gnomesort()
+    {
+        
+        
+        for(int i=0; i<=99999 ; i++){
+            Array[i] = i;
+            
+        }
+       
+        
+        long sta = System.nanoTime(); 
+        Gnomesort(Array);             
+        long sto = System.nanoTime(); 
+        long d2 = sto-sta;            
+        System.out.println(d2);       
+    }
+
+ 
+    public int[] Gnomesort(int []Array)
+    {
+        for(int index=1;index<Array.length;){
+            if(Array[index-1]<=Array[index]){
+                index++;
+            }    
+            else{ int remember = Array[index];
+                  Array[index] = Array[index-1];
+                  Array[index-1] = remember;
+                  index--;
+                  if(index == 0){
+                      index=1;
+                    }    
+                }
+            }    
+        return Array;    
+    }
+    
+    public int ZufallsZahl() {
+ 
+            int zufallZahl = (int)(Math.random()*100); 
+                                                       
+            
+            return zufallZahl;                         
+    }
+}
+    
+    
+    
+    
+    
+
